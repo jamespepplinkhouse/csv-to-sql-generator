f3998b2c50f52d2c9b48f3908924c0e4c771e26c
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2020-08-22 10:39:54 +1000

    ...

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,24 @@ const exec = async () => {
   const inputFile = `data/input.csv`
   const outputFile = `data/output.sql`
 
+  console.log(`\nReading CSV data from ${inputFile}`)
+  console.time('readFile')
   const data = await csv()
     .fromString(await fs.promises.readFile(inputFile, { encoding: 'utf-8' }))
     .on('error', err => {
       throw err
     })
+  console.timeEnd('readFile')
 
-  console.log(JSON.stringify(data))
+  console.log(`Found ${data.length} rows in CSV`)
 
   const sqlStatements = data.map(x => `TEST ${x.firstName} TEST;`)
 
+  console.log(`\nWriting SQL statements to ${outputFile}`)
+  console.time('writeFile')
   await fs.promises.writeFile(outputFile, sqlStatements.join('\n'))
+  console.timeEnd('writeFile')
+  console.log('\n')
 }
 
 exec().catch(console.error)
